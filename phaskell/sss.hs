a615a37532ee8829c4947f5fc5f5ca9576bd21aa
Philip Moss Robinson <probinson@localhost.localdomain>
2015-04-15 15:14:25 -0700

    use of liftM2 instead of writing explicit monad

--- phaskell/sss.hs
+++ phaskell/sss.hs
@@ -12,7 +12,8 @@
 
 import System.Random
 import Data.List
-
+import Control.Monad
+{-
 coeficients2poly :: (Num a) => [a] -> a -> a
 coeficients2poly cx = coeficients2value cx
 
@@ -26,7 +27,7 @@ randomList :: Int -> StdGen -> [Int]
 randomList n = take n . unfoldr (Just . random)
 
 msg2poly :: Int -> Int -> Int -> Int
-msg2poly degree message = 
+msg2poly degree message =
   let
     cs = randomList degree defaultGen
   in
@@ -47,7 +48,7 @@ main =
     points = msg2shares 4 7 1000
     message = points2message points
   in  putStrLn $ show message
-
+-}
 
 points2message :: (Eq b, Fractional b) => [(b, b)] -> b
 points2message points = points2poly points 0
@@ -61,11 +62,13 @@ points2poly points =
     \x -> foldr1 addFunctions ljs x
 
 addFunctions :: (Monad m, Num b) => m b -> m b -> m b
+addFunctions a b = liftM2 (+) a b
+{-
 addFunctions a b = do
   x <- a
   y <- b
   return (x+y)
-
+-}
 
 l :: (Eq a, Fractional a) => (a, a) -> [(a, a)] -> a -> a
 l point ps =
@@ -80,10 +83,7 @@ l' :: Fractional a => a -> [a] -> (a -> a)
 l' j mx = foldr1 multFunctions $ map (\m -> l'' j m) mx
 
 multFunctions :: (Monad m, Num b) => m b -> m b -> m b
-multFunctions a b = do
-  x <- a
-  y <- b
-  return (x*y)
+multFunctions a b = liftM2 (+*) a b
 
 
 l'' :: Fractional a => a -> a -> a -> a
