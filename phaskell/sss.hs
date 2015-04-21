960584e821b26171f0fb7177951689085124279e
Philip Moss Robinson <probinson@localhost.localdomain>
2015-04-20 19:03:39 -0700

    fromIntegral for points . logic seems wrong

--- phaskell/sss.hs
+++ phaskell/sss.hs
@@ -13,7 +13,7 @@
 import System.Random
 import Data.List
 import Control.Monad
-{-
+
 coeficients2poly :: (Num a) => [a] -> a -> a
 coeficients2poly cx = coeficients2value cx
 
@@ -43,12 +43,20 @@ msg2shares k n m =
     map (\x -> (x, p x)) xs
 
 
+shares2points :: [(Int, Int)] -> [(Rational, Rational)]
+shares2points = map share2point
+  where
+    -- share2point :: (Int, Int) -> (Rational, Rational)
+    share2point (a, b) = (fromIntegral a, fromIntegral b)
+
+
 main =
   let
-    points = msg2shares 4 7 1000
+    points = shares2points $ msg2shares 4 7 1000
     message = points2message points
   in  putStrLn $ show message
--}
+
+
 
 points2message :: (Eq b, Fractional b) => [(b, b)] -> b
 points2message points = points2poly points 0
@@ -63,12 +71,7 @@ points2poly points =
 
 addFunctions :: (Monad m, Num b) => m b -> m b -> m b
 addFunctions a b = liftM2 (+) a b
-{-
-addFunctions a b = do
-  x <- a
-  y <- b
-  return (x+y)
--}
+
 
 l :: (Eq a, Fractional a) => (a, a) -> [(a, a)] -> a -> a
 l point ps =
@@ -80,7 +83,7 @@ l point ps =
 
 
 l' :: Fractional a => a -> [a] -> (a -> a)
-l' j mx = foldr1 multFunctions $ map (\m -> l'' j m) mx
+l' j mx = foldr1 multFunctions $ map (l'' j) mx
 
 multFunctions :: (Monad m, Num b) => m b -> m b -> m b
 multFunctions a b = liftM2 (*) a b
